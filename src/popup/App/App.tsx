9b322921f99b28283e5c42c2591013235f4eddff
Ashwin Balaji S <ashwinbalaji@infinitisoftware.net>
2024-05-29 19:51:26 +0530

    first commit

---
+++ src/popup/App/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ProfilePic from "../assests/pic.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello World!</div>
+      <img src={ProfilePic}></img>
+    </div>
+  );
+}
+
+export default App;
